ae88bbf7868538a9c7e954bb2eef449e210070a7
zdzDesigner <1696498456@qq.com>
2021-10-05 21:21:38 +0800

    wp

--- dwm/scripts/wp-change.sh
+++ dwm/scripts/wp-change.sh
@@ -1,3 +1,57 @@
 #!/bin/bash
 
-feh --recursive --randomize --bg-fill $HOME/.bg/*
+#!/bin/bash
+
+if [[ ! -f ".temp" ]]; then
+    echo "" > .temp
+fi
+
+curbuf=$(cat .temp)
+echo $curbuf
+curnum=${curbuf%%@*}
+cururl=${curbuf##*@}
+curnum=${curnum:-0}
+
+echo $cururl
+echo $curnum
+
+function getwp() {
+    tarurl=""
+    declare -i count=0
+
+    for f in $HOME/.bg/*
+    do
+        if [ -f "$f" ];then
+            tarurl="${f##*/}"
+            if [[ $count == 1 ]]; then
+                echo "1@$tarurl" > .temp
+                break
+            fi
+            if [[ $1 == 0  ]]; then
+                echo "1@$tarurl" > .temp
+                break
+            fi
+            # echo ${f##*/}
+            if [[ $2 == $tarurl ]]; then
+                count=1
+            fi
+        fi
+    done
+    echo $tarurl
+}
+
+ret=$(getwp $curnum $cururl)
+echo ret: "$ret"
+if [[ $ret == $cururl ]]; then
+    echo "equl"
+    ret=$(getwp 0 $cururl)
+fi
+echo ret rest: $ret
+
+
+
+
+
+
+feh --bg-fill  $HOME/.bg/$ret
+# feh --recursive --randomize --bg-fill $HOME/.bg/*
