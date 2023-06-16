203334904939c4a54ae4c35513901c6cbb240d17
zdzDesigner <1696498456@qq.com>
2023-06-16 23:24:51 +0800

    ~

--- nvim/v8/lua/lsp/mason.lua
+++ nvim/v8/lua/lsp/mason.lua
@@ -27,6 +27,7 @@ local server_settings = {
   ['gopls'] = 0,
   ['clangd'] = 0,
   ['dockerls'] = 0,
+  ['asm_lsp'] = 0,
   -- ['yamlls'] = 0,
   ['sqlls'] = require('lsp/lang/sqlls'),
   -- ['ccls'] = require('lsp/ccls')
