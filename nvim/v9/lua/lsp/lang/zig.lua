e8edbd8a87bd82332bb75b1335d767a044f0bf14
zdzDesigner <1696498456@qq.com>
2024-04-25 18:53:18 +0800

    ~

--- nvim/v9/lua/lsp/lang/zig.lua
+++ nvim/v9/lua/lsp/lang/zig.lua
@@ -8,8 +8,10 @@ return {
   -- single_file_support = false, // false 支持库中跳转, 内存开销大
   settings = {
     zls = {
-      zig_exe_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0-dev.3439+31a7f22b8/zig',
-      zig_lib_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0-dev.3439+31a7f22b8/lib',
+      -- zig_exe_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0-dev.3439+31a7f22b8/zig',
+      -- zig_lib_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0-dev.3439+31a7f22b8/lib',
+      zig_exe_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0/zig',
+      zig_lib_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0/lib',
       -- build_on_save_step = "",
       -- enable_snippets = false,
       -- enable_argument_placeholders = false,
