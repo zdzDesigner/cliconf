41919c32eb992e384508ca63f77399eb70ee3403
zdzDesigner <1696498456@qq.com>
2024-04-26 23:16:04 +0800

    ~

--- nvim/v9/lua/lsp/lang/zig.lua
+++ nvim/v9/lua/lsp/lang/zig.lua
@@ -2,14 +2,16 @@ if not pcall(require, "lspconfig") then return end -- 保护调用加载模块
 -- local lspconfig = require('lspconfig')
 
 return {
+  -- cmd = { '/home/zdz/Application/zig/lsp/zls/zig-out/bin/zls' },
   cmd = { '/home/zdz/Application/zig/lsp/zls/zig-out/bin/zls' },
-  -- cmd = { '/home/zdz/Application/zig/lsp/zls.0.11.0/bin/zls' },
   filetypes = { "zig", "zon" },
   -- single_file_support = false, // false 支持库中跳转, 内存开销大
   settings = {
     zls = {
-      zig_exe_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0-dev.3439+31a7f22b8/zig',
-      zig_lib_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0-dev.3439+31a7f22b8/lib',
+      -- zig_exe_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0-dev.3439+31a7f22b8/zig',
+      -- zig_lib_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0-dev.3439+31a7f22b8/lib',
+      zig_exe_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0/zig',
+      zig_lib_path = '/home/zdz/Application/zig/zig-linux-x86_64-0.12.0/lib',
       -- build_on_save_step = "",
       -- enable_snippets = false,
       -- enable_argument_placeholders = false,
