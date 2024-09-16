4678dd64c34c24139f519452938db7cff9ab191d
zdzDesigner <1696498456@qq.com>
2024-09-16 10:12:44 +0800

    ~

--- dwm/lsusb_w.sh
+++ dwm/lsusb_w.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ret=1;
+throttle_end=1;
 # 定义节流函数
 function throttle() {
     local last_call_time_file="/tmp/last_call_time"
@@ -24,29 +24,78 @@ function throttle() {
     # if [ $time_diff -lt $wait_time ]; then
     if [ $time_diff -gt $wait_time ]; then
         # sleep $((wait_time - time_diff))
-        echo 0
+        echo true
     else
         # 更新上次调用时间
-        echo 1
+        echo false
     fi
     echo $current_time > "$last_call_time_file"
 }
+
+
+# 磁盘挂载 ===============================
+is_udisksctl=false
+if command -v udisksctlx &> /dev/null; then
+    is_udisksctl=true
+fi
+function mountDisk(){
+    if [ $is_udisksctl == false ]; then 
+      return 
+    fi
+    local is_disk=$1
+    if [ $is_disk == true ]; then
+        echo "添加USB设备$line"
+    else
+        echo "is_disk:$is_disk"
+    fi
+}
+# ========================================
+
+
+# 键盘 ===================================
+function quickKeyboard(){
+    local is_keyboard=$1
+    if [ $is_keyboard == true ]; then
+        quickey.sh
+        echo "添加USB设备:xy_3dg12"
+    else
+        echo "is_keyboard:$is_keyboard"
+    fi
+}
+# ========================================
+
+
+
 # 监控USB设备插入和移除
 udevadm monitor --subsystem-match=usb --property | while read -r line; do
     # echo "$line"
     # 检测到USB设备事件后运行lsusb
     if [[ "$line" == *"add"* || "$line" == *"remove"* ]]; then
-      ret=$(throttle 2)
-      # echo "vvvv-$ret"
-      if [ $ret == 0 ]; then
+
+      throttle_end=$(throttle 2)
+      # echo "vvvv-$throttle_end"
+      if [ $throttle_end == true ]; then
         sleep 1
         echo "USB设备更改检测到，更新设备列表："
-        lsusb | while read -r line; do
+
+        is_keyboard=false
+        is_disk=false
+        # 用管道 (|) 时，整个管道的输出会被送到子 shell 中执行 is_disk 在子shell中修改对当前无效
+        # lsusb | while read -r line; do
+        while read -r line; do
           if [[ "$line" == *"xy_3dg12"* ]]; then
-              quickey.sh
-              echo "添加USB设备:xy_3dg12"
+              is_keyboard=true
           fi
-        done
+          if [[ "$line" == *"Backup+ BK"* ]]; then
+              is_disk=true
+          fi
+        done < <(lsusb) 
+        # 业务 ================================
+        ## 键盘
+        quickKeyboard $is_keyboard
+        ## 挂载
+        mountDisk $is_disk
+
       fi
     fi
 done
