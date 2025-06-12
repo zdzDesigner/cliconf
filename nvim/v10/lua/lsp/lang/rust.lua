7ca77a3dce9f1751502a94d9d11a6bde8faea80c
zdzDesigner <1696498456@qq.com>
2025-06-12 22:25:14 +0800

    ~

--- nvim/v10/lua/lsp/lang/rust.lua
+++ nvim/v10/lua/lsp/lang/rust.lua
@@ -1,10 +1,25 @@
 return {
+  -- cmd = { "rust-analyzer" },
+  cmd = { "/home/zdz/Application/rust/lsp/rust-analyzer" },
+  filetypes = { 'rust' },
   settings = {
     ["rust-analyzer"] = {
+      -- checkOnSave = {
+      --   command = "clippy", -- 即使没有 Cargo.toml 也启用代码检查
+      -- },
+      -- standalone = true,
       diagnostics = {
-        disabled = { "unlinked-file" }, -- 禁用 unlinked-file 警告
+        disabled = { "unlinked-file", "unresolved-proc-macro" }, -- 禁用 unlinked-file 警告
+      },
+      procMacro = {
+        enable = true, -- 启用宏支持
+        attributes = {
+          enable = true,
+        },
+      },
+      buildScripts = {
+        enable = true,
       },
     },
   }
 }
-
